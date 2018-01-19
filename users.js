94ce75b9bfd4b05bf5b92007ba72b2588d7a250b
Distruttor <mirko.schicchi@studenti.unitn.it>
2018-01-19 17:07:07 +0100

    Add put route

--- users.js
+++ users.js
@@ -28,6 +28,20 @@ users.get('/:id', (req, res) => {
     }
 });
 
+users.put('/:id', (req, res) => {
+    const userID = req.params.id;
+    let username = req.body.username;
+    let password = req.body.password;
+    const i = usersChat.findIndex(u => {return u.userID === userID});
+    if(i==-1) {
+        res.sendStatus(404);
+    } else {
+        usersChat[i].username = username;
+        usersChat[i].password = password;
+        res.json(usersChat[i]);
+    }
+});
+
 
 module.exports = users;
 
