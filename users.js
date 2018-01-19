a1c93310b5fac23aba1b996626c12ba68b0331b5
Distruttor <mirko.schicchi@studenti.unitn.it>
2018-01-19 12:09:48 +0100

    Minor change

--- users.js
+++ users.js
@@ -6,11 +6,12 @@ const users = express.Router();
 const usersChat = [];
 
 users.post('/user', (req, res) => {
+    let userID = req.body.userID;
     let username = req.body.username;
     let password = req.body.password;
-    let registrationDate = new Date();
+    //let registrationDate = new Date();
 
-    let newUser = {username: username, password: password, registrationDate: registrationDate};
+    let newUser = {userID: userID, username: username, password: password};
 
     usersChat.push(newUser);
     res.json(newUser);
